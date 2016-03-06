76cdbc0b60edcd28c0fbfcf548a7b114901ade0a
Devin Rhode <devinrhode2@gmail.com>
2016-03-06 13:04:58 -0800

    flipperoo

--- app/contributions/edit/route.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-import SaveModelMixin from 'crowd-hero-frontend/mixins/contributions/save-model-mixin';
-
-export default Ember.Route.extend(SaveModelMixin, {
-});
--- app/mirage/factories/event.js
+++
@@ -1,5 +0,0 @@
-import Mirage from 'ember-cli-mirage';
-
-export default Mirage.Factory.extend(
-  {type: 'MyString', startTime: new Date(), endTime: new Date(), location: 'MyString', organizer: 'MyString', causes: [], contributors: 'MyString', beneficiary: 'MyString', eventImageSrc: 'MyString', description: 'MyString', name: 'MyString', expectedVolunteers: 'MyString' }
-);
